e9d0225b12c834d67a3958c6826cea637ec5cead
SinisterLuffy5 <151794064+SinisterLuffy5@users.noreply.github.com>
2024-12-10 14:09:59 -0500

    Update RunMap.bat

--- RunMap.bat
+++ RunMap.bat
@@ -1,5 +1,5 @@
 @echo off
 
-cd C:\Users\phung\Desktop\Lab\Academic_Records_files\Desktop\Yeah\Old\Tools\UjAPI\UjAPI 261\
+cd path\to\UjAPI\executable
 
-UjAPILauncher.exe -loadfile C:\Users\phung\Desktop\EO_UjAPI\_build\EO-UjAPI.w3x
+UjAPILauncher.exe -loadfile path\to\map
